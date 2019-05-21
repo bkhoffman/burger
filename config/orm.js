b936ca25dd71c9c00523b85059a7e763e450b162
boffman <bkhoffman@gmail.com>
2019-05-21 15:18:18 -0500

    finished file structure and setup

--- config/orm.js
+++ config/orm.js
@@ -1,27 +1,28 @@
 // Import MySQL connection.
 const connection = require("../config/connection.js");
 
-
-function selectAll(){
-  let queryString = "SELECT * FROM burgers_db";
-  connection.query(queryString, function(err, result){
-    if (err) throw err;
-    console.log(result);
+let orm = {
+  selectAll: function(whatToSelect, tableInput){
+    let queryString = "SELECT * FROM burgers";
+    connection.query(queryString, function(err, result){
+      if (err) throw err;
+      console.log(result);
+    })
+  },
+  insertOne: function(){
+    let queryString = "INSERT INTO burgers_db (burger_name) VALUES (?)";
+    connection.query(queryString, function(err, results){
+      if (err) throw err;
+      console.log(results);
   })
-};
-function insertOne(){
-  let queryString = "INSERT INTO burgers_db (burger_name) VALUES (?)";
-  connection.query(queryString, function(err, results){
-    if (err) throw err;
-    console.log(results);
+  },
+  updateOne: function(){
+    let queryString = "UPDATE burgers_db SET ? WHERE id = ?";
+    connection.query(queryString, function(err, results){
+      if (err) throw err;
+      console.log(results);
   })
-};
-function updateOne(){
-  let queryString = "UPDATE burgers_db SET ? WHERE id = ?";
-  connection.query(queryString, function(err, results){
-    if (err) throw err;
-    console.log(results);
-  })
-};
+  }
+}
 
 module.exports = orm;

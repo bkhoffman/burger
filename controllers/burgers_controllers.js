488f34a72cbb9b9b2ca180d9f4eb60e05637b299
boffman <bkhoffman@gmail.com>
2019-05-23 16:18:37 -0500

    page loading, css error

--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -9,7 +9,7 @@ router.get("/", function(req, res){
     let hbsObject = {
       burgers: data
     };
-    console.log("hbsObject" + hbsObject);
+    
     res.render("index", hbsObject);
   });
 });

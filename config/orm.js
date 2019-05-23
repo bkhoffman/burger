badfd4f254a0a013f1c49db106819a7aa6bc2b9b
boffman <bkhoffman@gmail.com>
2019-05-23 14:35:19 -0500

    style updates

--- config/orm.js
+++ config/orm.js
@@ -116,3 +116,43 @@ var orm = {
 
 //Export the orm object.
 module.exports = orm;
+
+//********************************* */
+// Import MySQL connection.
+// const connection = require("../config/connection.js");
+
+// let orm = {
+//   all: function(cb){
+//     let queryString = "SELECT * FROM burgers";
+//     connection.query(queryString, function(err, result){
+//       if (err) throw err;
+//       console.log("ORM: select all: " + result);
+//       cb(result);
+//     })
+//   },
+//   create: function(cb){
+//     let queryString = "INSERT INTO burgers_db (burger_name) VALUES (?)";
+//     connection.query(queryString, function(err, results){
+//       if (err) throw err;
+//       console.log(results);
+//       cb(result);
+//   })
+//   },
+//   update: function(cb){
+//     let queryString = "UPDATE burgers_db SET ? WHERE id = ?";
+//     connection.query(queryString, function(err, results){
+//       if (err) throw err;
+//       console.log(results);
+//       cb(result);
+//   })
+//   },
+//   delete: function(cb){
+//     let queryString = "DELETE FROM burgers WHERE " + condition;
+//     connection.query(queryString, function(err, results){
+//       if (err) throw err;
+//       cb(result);
+//     })
+//   }
+// }
+
+// module.exports = orm;

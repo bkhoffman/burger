741145e32f654bd18c3ee2ebbc581a373da87c2e
boffman <bkhoffman@gmail.com>
2019-05-31 16:22:45 -0500

    updated connection with jawsdb info

--- config/connection.js
+++ config/connection.js
@@ -1,22 +1,27 @@
 //mysql connection
 const mysql = require("mysql");
 //process.env.JAWSDB_URL
-const connection = mysql.createConnection({
-  host: process.env.HOST, 
-  port: 3306,
-  user: process.env.USERNAME,
-  password: process.env.BURGER_ID, 
-  database: process.env.DATABASE
-});
 
+if(process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}else{
+    connection = mysql.createConnection({
+    host: process.env.HOST, 
+    port: 3306,
+    user: process.env.USERNAME,
+    password: process.env.BURGER_ID, 
+    database: process.env.DATABASE
+  });
+}
+connection.connect();
 //make the connection
-connection.connect(function(err){
-  if(err){
-    console.log("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
+// connection.connect(function(err){
+//   if(err){
+//     console.log("error connecting: " + err.stack);
+//     return;
+//   }
+//   console.log("connected as id " + connection.threadId);
+// });
 
 //Export connection for our ORM to use.
 module.exports = connection;

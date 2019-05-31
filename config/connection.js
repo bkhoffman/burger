701bdd39527592d17603ebd5bd5840eaecff1cfa
boffman <bkhoffman@gmail.com>
2019-05-31 17:04:40 -0500

    updated development connection

--- config/connection.js
+++ config/connection.js
@@ -5,23 +5,14 @@ if(process.env.JAWSDB_URL){
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 }else{
     connection = mysql.createConnection({
-    host: process.env.HOST, 
+    host: process.env.DEVELOPMENT_HOST, 
     port: 3306,
-    user: process.env.USERNAME,
-    password: process.env.BURGER_ID, 
-    database: process.env.DATABASE
+    user: process.env.DEVELOPMENT_USERNAME,
+    password: process.env.DEVELOPMENT_PASSWORD, 
+    database: process.env.DEVELOPMENT_DATABASE
   });
 }
 connection.connect();
-//make the connection
-// connection.connect(function(err){
-//   if(err){
-//     console.log("error connecting: " + err.stack);
-//     return;
-//   }
-//   console.log("connected as id " + connection.threadId);
-// });
 
-//Export connection for our ORM to use.
 module.exports = connection;
 
